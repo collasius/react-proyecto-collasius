cad593b012d9751ac8940a0143b6ba479fd799bf
Pablo Collasius <pabloacollasius@hotmail.com>
2023-03-28 19:58:58 -0300

    creacion de componentes

---
+++ src/componentes/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Cartwidget from '../Cartwidget/Cartwidget'
+import Items from './Items/Items'
+
+const Navbar = () => {
+  return (
+    <div>
+
+      <Items/>
+      
+      <Cartwidget/>
+
+    </div>
+  )
+}
+
+export default Navbar
